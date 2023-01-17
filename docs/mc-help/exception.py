f497caa64deafe0e4bf12de119f710aa29657d15
The Great <64469119+Rajnish-TheGreat@users.noreply.github.com>
2023-01-17 17:21:04 +0530

    Modularized the framework (#3)
    * Added .github for sublinking
    
    Signed-off-by: Rajnishkumar Mishra <rajnish.mishra@seagate.com>
    
    * Updated workflow
    
    Signed-off-by: Rajnishkumar Mishra <rajnish.mishra@seagate.com>
    
    * Added Workflow
    
    Signed-off-by: Rajnishkumar Mishra <rajnish.mishra@seagate.com>
    
    * Add License text
    
    Signed-off-by: Rajnishkumar Mishra <rajnish.mishra@seagate.com>
    
    * Added Alex Reviewdog workflow
    
    Signed-off-by: Rajnishkumar Mishra <rajnish.mishra@seagate.com>
    
    * Added Alex Reviewdog Workflow
    
    Signed-off-by: Rajnishkumar Mishra <rajnish.mishra@seagate.com>
    
    * Added .gitignore  .whitesource  CODE_OF_CONDUCT.md
    
    Signed-off-by: Rajnishkumar Mishra <rajnish.mishra@seagate.com>
    
    * Modularised the framework
    
    Signed-off-by: Rajnishkumar Mishra <rajnish.mishra@seagate.com>
    
    Signed-off-by: Rajnishkumar Mishra <rajnish.mishra@seagate.com>
    Co-authored-by: Rajnishkumar Mishra <rajnish.mishra@seagate.com>

---
+++ docs/mc-help/exception.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2022 Seagate Technology LLC and/or its Affiliates
+
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License,
+# or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# For any questions about this software or licensing, please email
+# opensource@seagate.com
+
+import logging as LOG
+import configparser as CONF
+import six
+import sys
+
+
+class MCException(Exception):
+    message = 'An unknown exception occurred.'
+    code = 0
+
+    def __init__(self, message=None, code=0, **kwargs):
+        self.kwargs = kwargs
+        self.kwargs['message'] = message
+        self.code = int(code)
+
+        for k, v in self.kwargs.items():
+            if isinstance(v, Exception):
+                self.kwargs[k] = six.text_type(v)
+
+        if self._should_format():
+            try:
+                message = self.message % kwargs
+
+            except Exception:
+                exc_info = sys.exc_info()
+                # kwargs doesn't match a variable in the message
+                # log the issue and the kwargs
+                LOG.exception('Exception in string format operation')
+                for name, value in kwargs.items():
+                    LOG.error("%(name)s: %(value)s",
+                              {'name': name, 'value': value})
+                if CONF.fatal_exception_format_errors:
+                    six.reraise(*exc_info)
+                # at least get the core message out if something happened
+                message = self.message
+        elif isinstance(message, Exception):
+            message = six.text_type(message)
+
+        self.msg = message
+        super(MCException, self).__init__(message)
+
+    def _should_format(self):
+        return self.kwargs['message'] is None or '%(message)' in self.message
+
+    def __unicode__(self):
+        return self.msg
